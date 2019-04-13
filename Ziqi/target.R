d02bfe13e0360bab57e482ee1d76838c722584b4
ziqi56 <43015830+ziqi56@users.noreply.github.com>
2019-04-13 01:27:33 -0400

    Add files via upload
    remove duplicates

--- Ziqi/target.R
+++ Ziqi/target.R
@@ -1,12 +1,13 @@
 ## target array -- elapsed_workdays
 
 library(tidyverse)
-dat <- read_csv(file = 'merged_dcra_cama.csv', col_names = TRUE)
+dat <- read_csv(file = 'merged_dcra_cama.csv', col_names = TRUE) 
 
-# get a sense of types within variables of interest
-pid <- dat %>% 
-  group_by(perm_id) %>% 
-  summarise(n = n())
+# variables of interst
+pid <- dat %>%
+  group_by(perm_id) %>%
+  summarise(n=n())
+# duplicate rows arise
 
 status <- dat %>%
   group_by(TaskStatus) %>%
@@ -16,13 +17,25 @@ agency <- dat %>%
   group_by(AGENCY) %>%
   summarise(n=n())
 
+# assess duplicate rows
+fil <- dat %>%
+  filter(perm_id=='6014340')
+# only AYB,EYB,LIVING_GBA different
+
+dat <- dat %>%
+  group_by(perm_id) %>%
+  filter(row_number()== 1) # remove duplicates by keeping the first row for each unique perm_id
+# some rows in original permit_review dataset are duplicated after merging
+
 # according to DaaDicionary, it will be easier to interpert elapsed_workdays 
 # if TaskStatus=Complete and exclude customer operations
 dat <- dat %>%
   filter(TaskStatus=='Complete' & AGENCY != 'CUSTOMER')
 
+write.csv(dat,'dat.csv')
 #==============================
-# if we set perm_id as unit of analysis
-days_per_permit <- dat %>%
-  group_by(perm_id) %>%
+# if we set pdox id as unit of analysis
+days_per_pdox <- dat %>%
+  group_by(pdox_b1_id) %>%
   summarise(days = sum(elapsed_workdays))
+
